140a6189f22f0d5373162caed7ffc40760969fcb
Felipe Tsuyoshi Suzuki <ftsuyoshisuzuki@gmail.com>
2025-09-09 06:51:32 -0300

    Configuração e criação da primeira migration

--- Data/ApiDbContext.cs
+++ Data/ApiDbContext.cs
@@ -1,10 +1,14 @@
 using System;
 using Microsoft.EntityFrameworkCore;
+using StudyApi.Models;
 
 namespace StudyApi.Data;
 
 public class ApiDbContext : DbContext
 {
-    
+    public ApiDbContext(DbContextOptions<ApiDbContext> options) : base(options)
+    {
 
+    }
+    public DbSet<Produto> Produtos { get; set; }
 }
