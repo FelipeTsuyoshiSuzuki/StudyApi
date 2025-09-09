67f15f68834646d169bd310d8241cbefdafeacac
Felipe Tsuyoshi Suzuki <ftsuyoshisuzuki@gmail.com>
2025-09-09 06:24:12 -0300

    Adição de dependências e criação do model produto

---
+++ Models/Produto.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace StudyApi.Models;
+
+public class Produto
+{
+    public int Id { get; set; }
+    public string? Nome { get; set; }
+    public decimal Preco { get; set; }
+    public int QuantidadeEstoque { get; set; }
+    public string? Descricao { get; set; }
+
+}
